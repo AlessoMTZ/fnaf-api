1370f76ac70bd1bbdea9b5a2bb3ca8bfc30f7cf2
AlessoMTZ <gastonmetzgera@gmail.com>
2023-11-13 15:33:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,56 @@
-const express = require('express')
-const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express();
 app.use(express.json());
 
-const animatronicos = [
-    {
-        "id":1,
-        "nombre": "Freddy Fazbear",
-        "descripcion": "El anfitrión principal de Freddy Fazbear's Pizza.",
-        "tipo": "Oso animatrónico",
-        
-    },
-    {
-        "id":2,
-        "nombre": "Chica",
-        "descripcion": "Una gallina animatrónica que canta en el escenario.",
-        "tipo": "Animatrónica de ave",
-        
-    },
-    {
-        "id":3,
-        "nombre": "Foxy",
-        "descripcion": "Un pirata animatrónico que acecha en el pasillo.",
-        "tipo": "Animatrónico de zorro",
-        
-    }
-];
-app.get('/', (req,res) => {
-    res.send('Fnaf Api');
-})
-app.get('/api/animatronicos', (req,res) => {
-    res.send(animatronicos);
-})
-app.get('/api/animatronicos/:id', (req,res) => {
-    const animatronico = animatronicos.find(c => c.id === parseInt(req.params.id));
-    if(!animatronico) return res.status(404).send('Animatronico no encontrado')
-    else res.send(animatronico);
-})
+// Conectar a la base de datos MongoDB
+mongoose.connect('mongodb://localhost/fnafDB', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conectado a MongoDB...'))
+    .catch(err => console.error('Error al conectar a MongoDB:', err));
 
-app.post('/api/animatronicos', (req,res) => {
-    const animatronico = {
-        
-        id: animatronicos.length + 1,
-        nombre: req.body.nombre,
-        descripcion: req.body.descripcion,
-        tipo: req.body.habilidades,
-        
-    };
-    animatronicos.push(animatronico);
-    res.send(animatronico);
-})
-app.delete('/api/animatronicos/:id', (req,res) => {
-    const animatronico = animatronicos.find(c => c.id === parseInt(req.params.id));
-    if(!animatronico) return res.status(404).send('Animatronico no encontrado')
-    
-    const index = animatronico.indexOf(animatronico);
-    animatronicos.splice(index, 1);
-    res.send(animatronico);
-
+// Definir el esquema del modelo Animatronico
+const animatronicoSchema = new mongoose.Schema({
+    nombre: String,
+    descripcion: String,
+    tipo: String
 });
 
-const port = process.env.port || 80;
-app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));
+// Crear el modelo Animatronico
+const Animatronico = mongoose.model('Animatronico', animatronicoSchema);
+
+// Rutas de la API
+app.get('/', (req, res) => {
+    res.send('Fnaf Api');
+});
+
+app.get('/api/animatronicos', async (req, res) => {
+    const animatronicos = await Animatronico.find();
+    res.send(animatronicos);
+});
+
+app.get('/api/animatronicos/:id', async (req, res) => {
+    const animatronico = await Animatronico.findById(req.params.id);
+    if (!animatronico) return res.status(404).send('Animatronico no encontrado');
+    res.send(animatronico);
+});
+
+app.post('/api/animatronicos', async (req, res) => {
+    const animatronico = new Animatronico({
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        tipo: req.body.tipo
+    });
+
+    await animatronico.save();
+    res.send(animatronico);
+});
+
+app.delete('/api/animatronicos/:id', async (req, res) => {
+    const animatronico = await Animatronico.findByIdAndRemove(req.params.id);
+    if (!animatronico) return res.status(404).send('Animatronico no encontrado');
+    res.send(animatronico);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Escuchando en el puerto ${port}...`));
